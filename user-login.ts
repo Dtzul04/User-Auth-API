576a9cee2e2b6455e453da7a66be86ce407399d4
Daniel Tzul <dtzul8@gmail.com>
2026-07-26 19:23:35 -0500

    WIP: register done, login in progress

--- user-login.ts
+++ user-login.ts
@@ -26,11 +26,50 @@ app.get("/", (req, res) => {
 })
 
 app.post("/register", async (req, res) => {
-    // Extract user data from request body
+    // Extract email and password from request body
+    const { email, password } = req.body;
+
+    // Validate email and password
+    if (!email || !password) {
+        return res.status(400).json({ error: "Email and password are required"});
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    
+    // Insert user into database
+    const result = await pool.query(
+        `INSERT INTO users (email, password_hash)
+        VALUES ($1, $2)
+        RETURNING id, email, created_at`,
+        [email, hashedPassword]
+        // Return success response
+    ); return res.status(201).json({
+        message: "User registered",
+        user: result.rows[0],
+    });
 })
 
 app.post("/login", async (req, res) => {
     // Extract email and password from request body
+    const { email, password} = req.body;
+
+    // Retrieve user from database
+     const { rows } = await pool.query(
+        `SELECT id, email, password_hash
+        FROM users
+        WHERE email = $1
+        `, [email]
+    ); 
+
+    const user = rows[0];
+    
+    if (!user) {
+        return res.status(401).json({ error: "Invalid email or password"});
+    }
+
+    const passwordMatches = await bcrypt.compare(password, user.password_hash);
+    // STEP #3
 })
 
 // Start the server
